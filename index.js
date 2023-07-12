0cee9dcb46b80ca7050672206493bc02e4752f2a
Riddhish Khot <riddhish.k@somaiya.edu>
2023-07-13 00:34:45 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT=8080;
+
+app.use(express.json());
+
+app.listen(
+    PORT,
+    ()=> console.log("Server Start")
+)
+
+app.get('/request',(req,res)=>{
+    res.status(200).send({
+        a:"abc"
+    })
+})
+
+
+app.post('/hi/:id',(req,res)=>{
+    const { info } = req.params;
+    const {body} = req.body;
+
+    res.status(200).send({
+        hi:`${info}`
+    })
+})
